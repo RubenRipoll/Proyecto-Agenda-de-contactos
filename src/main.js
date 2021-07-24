cbd65c46bd3d3c6313c2c2b77b58cfcd502c9eb3
RubenRipoll <ripoll45@outlook.com>
2021-07-24 01:37:35 -0300

    Completado100

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// Capturamos los objetos del DOM ↓
+const nombre = document.querySelector('.nombre')
+const numero = document.querySelector('.numero')
+const direccion = document.querySelector('.direccion')
+const btnAgregarTarea = document.querySelector('.btn-agregar-tarea')
+
+const listadoTareas = document.querySelector('.listado-tareas')
+
+// Accedemos al localStorage ↓
+const db = window.localStorage
+
+// Creamos el evento "click" y el objeto "contacto" ↓
+btnAgregarTarea.onclick = () => {
+    let contacto = {
+        id: Math.random(1, 100),
+        nombre: nombre.value,
+        numero: numero.value,
+        direccion: direccion.value,
+    }
+    guardarContacto(db, contacto) 
+}
+
+cargarContactos(db, listadoTareas)
+
+
+
